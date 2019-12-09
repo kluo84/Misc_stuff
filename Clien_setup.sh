e68844d9275f25adfb8576d78a72fd663b1eeb7d
kluo84 <kluo@localhost.localdomain>
2019-12-09 10:26:41 -0600

    update client script

--- Clien_setup.sh
+++ Clien_setup.sh
@@ -2,9 +2,8 @@ apt update -y
 apt install git tmux vim unzip -y
 wget https://raw.githubusercontent.com/kluo84/Misc_stuff/master/tmux.conf -O ~/.tmux.conf
 cd ~
-wget -O ~/.vim-config https://github.com/kluo84/vim-config .vim-config
-ln -s ~/.vim-config/.vim
-ln -s ~/.vim-config/.vimrc
+git clone https://github.com/kluo84/vim-prep.git ~/.vim-prep
+ln -s ~/.vim-prep/vim-prep/.vim
+ln -s ~/.vim-config/vim-prep/.vimrc
 
 #set :PlugInstall!
-vim
