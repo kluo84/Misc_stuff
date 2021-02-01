0e6c679721451d50d4fa25f8838b58fd5a222b5d
kluo <15222944+kluo84@users.noreply.github.com>
2021-02-01 10:04:07 -0600

    Update fix_vmware_hgfs_share.sh

--- fix_vmware_hgfs_share.sh
+++ fix_vmware_hgfs_share.sh
@@ -1,3 +1,4 @@
-git clone https://github.com/rasa/vmware-tools-patches.git
-cd vmware-tools-pathes
-./patched-open-vm-tools.sh
+#git clone https://github.com/rasa/vmware-tools-patches.git
+#cd vmware-tools-pathes
+#./patched-open-vm-tools.sh
+vmhgfs-fuse .host:/ /mnt/hgfs/ -o allow_other -o uid=1000
